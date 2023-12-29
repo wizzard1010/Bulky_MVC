602618cc4e6b2f3458c9550dd0844bb239df3128
Teddy <apple@APPLEs-MacBook-Air.local>
2023-12-29 22:15:46 +0630

    Repository implementation

---
+++ Bulky.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bulky.Models;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+	public interface ICategoryRepository : IRepository<Category>
+	{
+		void Update(Category obj);
+		void Save();
+	}
+}
+
---
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Bulky.DataAccess.Repository.IRepository
+{
+	public interface IRepository<T> where T : class
+	{
+		//T - Category
+		IEnumerable<T> GetAll();
+		T Get(Expression<Func<T, bool>> filter);
+		void Add(T entity);
+        void Remove(T entity);
+		void RemoveRange(IEnumerable<T> entity);
+    }
+}
+
